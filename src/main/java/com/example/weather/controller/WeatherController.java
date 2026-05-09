79a9065f06e39a8346537f2a089afe13cbb8aeac
Leonid <lemalaxz2@gmail.com>
2026-05-09 20:00:31 +0200

    added all endpoint for

--- src/main/java/com/example/weather/controller/WeatherController.java
+++ src/main/java/com/example/weather/controller/WeatherController.java
@@ -3,8 +3,10 @@ package com.example.weather.controller;
 import com.example.weather.dto.WeatherResponse;
 import com.example.weather.service.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/weather")
